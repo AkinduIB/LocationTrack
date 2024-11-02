642d7232ac16ef6d5754a60b05196a0c89c60bbd
Akindu Bandaranayake <avdgroup185@gmail.com>
2024-11-02 14:53:05 +0530

    updates

--- BackEnd/backend.js
+++ BackEnd/backend.js
@@ -1,4 +1,3 @@
-// backend.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,21 +6,17 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-let userLocations = {}; // Store for user locations
+let userLocations = {};
 
-// Endpoint to update location
 app.post('/updateLocation', (req, res) => {
     const { userId, latitude, longitude } = req.body;
     userLocations[userId] = { latitude, longitude, timestamp: new Date() };
     res.send({ message: 'Location updated' });
 });
 
-// Endpoint to retrieve a user's location
 app.get('/getLocation/:userId', (req, res) => {
     const { userId } = req.params;
     res.send(userLocations[userId] || { message: 'No location found for user' });
 });
 
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
+module.exports = app;
